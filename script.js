8bbfd93fe0b35dcddbe04eb446bd449234c58ef1
Alex Nye <galexynye@gmail.com>
2018-01-12 12:49:09 +0100

    Initial commit with the css grid not backward compatable

---
+++ script.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+       lightbox.option({
+      'resizeDuration': 900,
+      'wrapAround': true,
+       
+    })
+//   $('#lightSlider').lightSlider({
+//    gallery: true,
+//    item: 1,
+//    loop:true,
+//    slideMargin: 0,
+//    thumbItem: 3,
+//    pager: true,
+       
+//      item: 3,
+//        autoWidth: false,
+//        slideMove: 1, // slidemove will be 1 if loop is true
+//        slideMargin: 10,
+// 
+//        addClass: '',
+//        mode: "slide",
+//        useCSS: true,
+//        cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
+//        easing: 'linear', //'for jquery animation',////
+// 
+//        speed: 400, //ms'
+//        auto: false,
+//        loop: false,
+//        slideEndAnimation: true,
+//        pause: 2000,
+// 
+//        keyPress: false,
+//        controls: true,
+//        prevHtml: '',
+//        nextHtml: '',
+// 
+//        rtl:false,
+//        adaptiveHeight:false,
+// 
+//        vertical:false,
+//        verticalHeight:500,
+//        vThumbWidth:100,
+// 
+//        thumbItem:10,
+//        pager: true,
+//        gallery: false,
+//        galleryMargin: 5,
+//        thumbMargin: 5,
+//        currentPagerPosition: 'middle',
+// 
+//        enableTouch:true,
+//        enableDrag:true,
+//        freeMove:true,
+//        swipeThreshold: 40,
+// 
+//        responsive : [],
+// 
+//        onBeforeStart: function (el) {},
+//        onSliderLoad: function (el) {},
+//        onBeforeSlide: function (el) {},
+//        onAfterSlide: function (el) {},
+//        onBeforeNextSlide: function (el) {},
+//        onBeforePrevSlide: function (el) {}  
+//    
+ //  });
+});
